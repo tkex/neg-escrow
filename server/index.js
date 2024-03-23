09df71fe33a974241fe292a984225e2886e48400
tkex <tikado@web.de>
2024-03-23 02:36:21 +0100

    chore: add comments to userSchema

--- server/index.js
+++ server/index.js
@@ -23,16 +23,19 @@ mongoose.connect(MONGOURL).then(() => {
 });
 
 const userSchema = new mongoose.Schema({
+    // Email vom Nutzer
     email: {
         type: String,
         required: true,
         unique: true
     },
+    // Benutzername vom Nutzer
     username: {
         type: String,
         required: true,
         unique: true
     },
+    // Passwort vom Nutzer
     password: {
         type: String,
         required: true
