997637fe814229f178a9b77b946ace205c08ba55
tkex <tikado@web.de>
2024-04-04 09:43:10 +0200

    chore: set private route to root

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -5,7 +5,7 @@ import { useAuth } from "../contexts/AuthContext.js";
 
 const PrivateRoute = () => {
   const { user } = useAuth();
-  return user ? <Outlet /> : <Navigate to="/login" />;
+  return user ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
