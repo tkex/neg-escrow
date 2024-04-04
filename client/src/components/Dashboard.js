bca74b13c7b820d33520d6dab80519ac693f4ec1
tkex <tikado@web.de>
2024-04-04 11:13:02 +0200

    feat: initial table layout and dashboard styling

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,6 +7,8 @@ import UserLastDeniedTrades from './UserLastDeniedTrades';
 import UserConfirmedTrades from './UserConfirmedTrades';
 import UserLastClosedTrades from './UserLastClosedTrades';
 
+import { FaPlus, FaMinus } from 'react-icons/fa';
+
 // Hook für das Abrufen der Handelszahlen
 const useTradeCount = (url, token) => {
   const [count, setCount] = useState(0);
@@ -27,24 +29,32 @@ const Dashboard = () => {
   const { user, logout, token } = useAuth();
   const [isTradeModalOpen, setIsTradeModalOpen] = React.useState(false);
 
+   // Zustände für das Ausklappen der Tabellen
+   const [showOpenTrades, setShowOpenTrades] = useState(false);
+  // Handler für das Ausklappen
+  const toggleOpenTrades = () => setShowOpenTrades(!showOpenTrades);
+
  // Hook, um die Anzahlen abzurufen
  const openTradesCount = useTradeCount('http://localhost:8000/trades/count/open', token);
  const closedTradesCount = useTradeCount('http://localhost:8000/trades/count/closed', token);
  const totalTradesCount = useTradeCount('http://localhost:8000/trades/count/total', token);
 
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Benutzername: {user ? user.username : 'Unbekannter Nutzer'}</p>
-      <p>Benutzer-ID: {user ? user.id : 'Unbekannte ID'}</p>
-      <div>
-        <p>Anzahl offener Verhandlungen: {openTradesCount}</p>
-        <p>Anzahl geschlossener Verhandlungen: {closedTradesCount}</p>
-        <p>Gesamtzahl der Verhandlungen im System: {totalTradesCount}</p>
-      </div>
-      <button onClick={logout}>Logout</button>
-      <hr />
-      <button onClick={() => setIsTradeModalOpen(true)}>Neue Verhandlung starten</button>
+ return (
+<div className="max-w-7xl mx-auto my-10 p-5 border rounded-lg shadow-lg">
+  <h1 className="text-4xl font-bold mb-4 text-center">Dashboard</h1>
+  <div className="mb-6 bg-gray-100 p-4 rounded">
+    <p className="mb-2 text-lg">Benutzername: <span className="font-semibold">{user ? user.username : 'Unbekannter Nutzer'}</span></p>
+    <p className="mb-4 text-lg">Benutzer-ID: <span className="font-semibold">{user ? user.id : 'Unbekannte ID'}</span></p>
+    
+    <p className="mb-2 text-lg">Anzahl offener Verhandlungen: <span className="font-semibold">{openTradesCount}</span></p>
+    <p className="mb-2 text-lg">Anzahl geschlossener Verhandlungen: <span className="font-semibold">{closedTradesCount}</span></p>
+    <p className="text-lg">Gesamtzahl der Verhandlungen: <span className="font-semibold">{totalTradesCount}</span></p>
+  </div>
+
+  <div className="flex flex-col mb-5 items-center">
+    <button onClick={logout} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors mb-10">Ausloggen</button>
+    <button onClick={() => setIsTradeModalOpen(true)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors w-72">Neue Verhandlung starten</button>
+  </div>
       
       {isTradeModalOpen && (
         <CreateNewTradeButton
@@ -54,16 +64,14 @@ const Dashboard = () => {
           }}
         />
       )}
-      <hr />
-      <OpenTrades token={token} userId={user?.id} />
-      <hr />
-      <GlobalLastTrades token={token} />
-      <hr />
-      <UserConfirmedTrades token={token} />
-      <hr />
-      <UserLastDeniedTrades token={token} />   
-      <hr />
-      <UserLastClosedTrades token={token} />      
+      
+      <div className="mt-8">
+        <OpenTrades token={token} userId={user?.id} />
+        <GlobalLastTrades token={token} />
+        <UserConfirmedTrades token={token} />
+        <UserLastDeniedTrades token={token} />   
+        <UserLastClosedTrades token={token} />
+      </div>
     </div>
   );
 };
--- client/src/components/UserConfirmedTrades.js
+++ client/src/components/UserConfirmedTrades.js
@@ -20,6 +20,7 @@ const UserAcceptedTrades = ({ token }) => {
     })
     .then(data => {
       // Kein Filter der Daten notwendig, da Backend direkt akzeptierte Trades liefert
+      // TODO: Eventuell in Zukunft dass nur die 100 letzten Trades angezeigt werden
       setAcceptedTrades(data);
     })
     .catch(error => console.error('Fehler beim Abrufen der akzeptierten Trades:', error));
@@ -27,44 +28,48 @@ const UserAcceptedTrades = ({ token }) => {
 
   return (
     <div>
-      <h2>Anzeige aller akzeptierten Verhandlungen des Benutzers:</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Datum</th>
-            <th>Uhrzeit der Erstellung</th>
-            <th>Betreff</th>
-            <th>Initiales Angebot</th>
-            <th>Geeinigter Preis</th>
-            <th>Gegenangebot-Historie</th>
-            <th>Käufer (ID)</th>
-            <th>Verkäufer (ID)</th>
-            <th>Status</th>
-            <th>Aktion</th>
-          </tr>
-        </thead>
-        <tbody>
-          {acceptedTrades.map(trade => (
-            <tr key={trade._id}>
-              <td>{new Date(trade.createdAt).toLocaleDateString()}</td>
-              <td>{new Date(trade.createdAt).toLocaleTimeString()}</td>
-              <td>{trade.subject}</td>
-              <td>{`${trade.initOffer.toFixed(2)}€`}</td>
-              <td>{trade.acceptedPrice ? `${trade.acceptedPrice.toFixed(2)}€` : 'N/A'}</td>
-              <td>{trade.offerHistory.map(offer => `${offer.toFixed(2)}€`).join(' → ')}</td>
-              <td>{trade.sender.username}</td>
-              <td>{trade.receiver.username}</td>
-              <td>{translateStatus(trade.status)}</td>
-              <td>
-                {/* Modal-Komponente einfügen  */}
-                <button onClick={() => console.log(`Details für Trade ID: ${trade._id}`)}>Details anzeigen</button>
-              </td>
+      <h2 className="text-lg font-semibold mb-4">Anzeige aller akzeptierten Verhandlungen des Benutzers:</h2>
+      <div className="overflow-x-auto relative shadow-lg sm:rounded-lg">
+        <table className="w-full text-sm text-gray-700">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-100">
+            <tr>
+              <th scope="col" className="px-6 py-3">Trade-ID</th>
+              <th scope="col" className="px-6 py-3">Datum</th>
+              <th scope="col" className="px-6 py-3">Uhrzeit der Erstellung</th>
+              <th scope="col" className="px-6 py-3">Betreff</th>
+              <th scope="col" className="px-6 py-3">Initiales Angebot</th>
+              <th scope="col" className="px-6 py-3">Geeinigter Preis</th>
+              <th scope="col" className="px-6 py-3">Gegenangebot-Historie</th>
+              <th scope="col" className="px-6 py-3">Käufer (ID)</th>
+              <th scope="col" className="px-6 py-3">Verkäufer (ID)</th>
+              <th scope="col" className="px-6 py-3">Status</th>
+              <th scope="col" className="px-6 py-3">Aktion</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {acceptedTrades.map(trade => (
+              <tr key={trade._id} className="bg-white border-b hover:bg-gray-50">
+                <td className="px-6 py-4">{trade._id.length > 7 ? `${trade._id.substring(0, 10)}...` : trade._id}</td>
+                <td className="px-6 py-4">{new Date(trade.createdAt).toLocaleDateString()}</td>
+                <td className="px-6 py-4">{new Date(trade.createdAt).toLocaleTimeString()}</td>
+                <td className="px-6 py-4">{trade.subject}</td>
+                <td className="px-6 py-4">{`${trade.initOffer.toFixed(2)}€`}</td>
+                <td className="px-6 py-4">{trade.acceptedPrice ? `${trade.acceptedPrice.toFixed(2)}€` : 'N/A'}</td>
+                <td className="px-6 py-4">{trade.offerHistory.map(offer => `${offer.toFixed(2)}€`).join(' → ')}</td>
+                <td className="px-6 py-4">{trade.sender.username}</td>
+                <td className="px-6 py-4">{trade.receiver.username}</td>
+                <td className="px-6 py-4 text-green-600 font-semibold">{translateStatus(trade.status)}</td>
+                <td className="px-6 py-4 text-right">
+                  <button onClick={() => console.log(`Details für Trade ID: ${trade._id}`)} className="font-medium text-blue-600 hover:underline">Details anzeigen</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
+
 
 export default UserAcceptedTrades;
